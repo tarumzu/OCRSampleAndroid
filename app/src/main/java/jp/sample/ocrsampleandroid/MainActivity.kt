f5e1fb83bef567f64e30b35871df6381888eedac
tarumzu <wwww.0909mizu@gmail.com>
2019-05-13 20:50:25 +0900

    fix OCR

--- app/src/main/java/jp/sample/ocrsampleandroid/MainActivity.kt
+++ app/src/main/java/jp/sample/ocrsampleandroid/MainActivity.kt
@@ -2,11 +2,87 @@ package jp.sample.ocrsampleandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.graphics.BitmapFactory
+import android.graphics.Bitmap
+import android.content.Intent
+import android.app.Activity
+import android.graphics.Matrix
+import androidx.exifinterface.media.ExifInterface
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST = 0
+    lateinit var ocrUtil: OCRUtil
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ocrUtil = OCRUtil(applicationContext)
+        button.setOnClickListener {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+            intent.addCategory(Intent.CATEGORY_OPENABLE)
+
+            intent.type = "image/*"
+            startActivityForResult(intent, REQUEST)
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST) {
+            if (resultCode == Activity.RESULT_OK && data != null) {
+                var bitmapOrigin: Bitmap? = null
+                var bitmap: Bitmap? = null
+
+                val uri = data.data
+                try {
+                    uri?.let {
+
+                        contentResolver.openFileDescriptor(it, "r").use {parcelFileDescriptorNullable ->
+                            parcelFileDescriptorNullable?.let {parcelFileDescriptor ->
+                                val fileDescriptor = parcelFileDescriptor.fileDescriptor
+                                bitmapOrigin = BitmapFactory.decodeFileDescriptor(fileDescriptor)
+                                // 画像の横、縦サイズを取得
+                                contentResolver.openInputStream(it).use {
+                                    it?.let {
+                                        var degrees = 0f
+                                        val exifInterface = ExifInterface(it)
+                                        val orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED)
+                                        when (orientation) {
+                                            1 -> {degrees = 0f}
+                                            3-> {degrees = 180f}
+                                            6 -> {degrees = 90f}
+                                            8 -> {degrees = 270f}
+                                        }
+                                        val matrix = Matrix()
+                                        val imageWidth = bitmapOrigin?.getWidth() ?: 0
+                                        val imageHeight = bitmapOrigin?.getHeight() ?: 0
+                                        matrix.setRotate(degrees, imageWidth.toFloat() / 2, imageHeight.toFloat() / 2)
+                                        bitmap = Bitmap.createBitmap(bitmapOrigin!!, 0, 0, imageWidth, imageHeight, matrix, true)
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+
+                bitmap?.let {
+                    image.setImageBitmap(it)
+                    ocr.text = ocrUtil.getString(applicationContext, it, OCRUtil.Companion.LangType.getLangType("").str)
+                } ?: run {
+                    ocr.text = "bitmap is null"
+                }
+            } else {
+                ocr.text = "system error"
+            }
+        }
     }
 }
---
+++ app/src/main/java/jp/sample/ocrsampleandroid/OCRUtil.kt
@@ -0,0 +1,95 @@
+package jp.sample.ocrsampleandroid
+
+import android.content.Context
+import com.googlecode.tesseract.android.TessBaseAPI
+import android.graphics.Bitmap
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
+
+class OCRUtil(context: Context) {
+
+    private val TESS_DATA_DIR = "tessdata" + File.separator
+    private val TESS_TRAINED_DATA = arrayListOf("eng.traineddata", "jpn.traineddata")
+
+    init {
+        checkTrainedData(context)
+    }
+
+    private fun checkTrainedData(context: Context) {
+        val dataPath = context.filesDir.toString() + File.separator + TESS_DATA_DIR
+        val dir = File(dataPath)
+        if (!dir.exists() && dir.mkdirs()) {
+            copyFiles(context)
+        }
+        if (dir.exists()) {
+            TESS_TRAINED_DATA.forEach {
+                val dataFilePath = dataPath + it
+                val datafile = File(dataFilePath)
+                if (!datafile.exists()) {
+                    copyFiles(context)
+                }
+            }
+        }
+    }
+
+    private fun copyFiles(context: Context) {
+        try {
+            TESS_TRAINED_DATA.forEach {
+                val filePath = context.filesDir.toString() + File.separator + TESS_DATA_DIR + it
+
+                context.assets.open(TESS_DATA_DIR + it).use {inputStream ->
+                    FileOutputStream(filePath).use {outStream ->
+                        val buffer = ByteArray(1024)
+                        var read = inputStream.read(buffer)
+                        while (read != -1) {
+                            outStream.write(buffer, 0, read)
+                            read = inputStream.read(buffer)
+                        }
+                        outStream.flush()
+                    }
+                }
+
+                val file = File(filePath)
+                if (!file.exists()) throw FileNotFoundException()
+            }
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun getString(context: Context, bitmap: Bitmap, lang: String): String {
+        val baseApi = TessBaseAPI()
+        baseApi.init(context.getFilesDir().toString(), lang)
+        baseApi.setImage(bitmap)
+        val recognizedText = baseApi.utF8Text
+        baseApi.end()
+
+        return recognizedText
+    }
+
+    companion object {
+        enum class LangType constructor(val str: String) {
+            jpn("jpn"),
+            eng("eng"),
+            UNKNOWN("eng");
+
+
+            companion object {
+
+                fun getLangType(str: String): LangType {
+                    val types = LangType.values()
+                    for (type in types) {
+                        if (type.str.equals(str)) {
+                            return type
+                        }
+                    }
+                    return UNKNOWN
+                }
+            }
+        }
+    }
+}
