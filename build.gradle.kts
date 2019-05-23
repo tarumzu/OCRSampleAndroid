b9cc103e3de051f882cc2d9d102727a082753e7d
tarumzu <wwww.0909mizu@gmail.com>
2019-05-23 19:40:34 +0900

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.4.1")
+        classpath("com.android.tools.build:gradle:3.5.0-beta02")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
 }
